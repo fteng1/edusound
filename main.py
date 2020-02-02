1650b950993e57d5d3992db919a2dc7e792fdde9
Alexandra Lai <alexandralai00@gmail.com>
2020-02-01 21:09:29 -0800

    main.py adds

--- main.py
+++ main.py
@@ -98,20 +98,20 @@ def check_profile_exists(value):
         my_profile.put()
     return my_profile
 
-
-
-
-
-
-
-
+class DeleteHandler(webapp2.RequestHandler):
+    def post(self):
+        subject_id = self.request.get('subject_id')
+        subject_key = ndb.Key(urlsafe = subject_id)
+        subject_key.delete()
+        time.sleep(0.1)
+        self.redirect('/subject')
 
 
 #https://www.dw.com/image/48688022_303.jpg
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/inputNotes', InputNotesPage),
-    ('/inputMusic', InputMusicPage),
+    ('/input', InputNotesPage),
     ('/subject', SubjectNotesPage),
     ('/songs', InputMusicPage)
+    ('/delete', DeleteHandler)
 ], debug=True)
